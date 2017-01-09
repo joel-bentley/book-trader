8d02f3881b74f6090bd701cde08a3e8d433e4611
Joel Bentley <joel-bentley@users.noreply.github.com>
2017-01-09 17:32:53 -0500

    Add react router code

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router'
+import App from './App'
+import './index.css'
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render((
+  <BrowserRouter>
+    {({ router }) => (
+      <App {...{ router }} />
+    )}
+  </BrowserRouter>
+),
   document.getElementById('root')
-);
+)
