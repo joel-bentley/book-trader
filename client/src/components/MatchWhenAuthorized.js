ab624b317387279c6ba024f3d2af0ac3bc93e493
Joel Bentley <joel-bentley@users.noreply.github.com>
2017-01-09 17:36:12 -0500

    Add custom components that will be useful

---
+++ client/src/components/MatchWhenAuthorized.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Match, Redirect } from 'react-router'
+
+const MatchWhenAuthorized = ({ render: Component, isAuthenticated, ...rest }) => (
+  <Match {...rest} render={props => (
+    isAuthenticated ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to={{
+        pathname: '/login',
+        state: { referrer: props.location }
+      }}/>
+    )
+  )}/>
+)
+
+export default MatchWhenAuthorized
