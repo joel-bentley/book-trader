538dcecc3ed64b123dee44e3e5a493c86bc0902d
Joel Bentley <joel-bentley@users.noreply.github.com>
2017-01-20 12:43:16 -0500

    Add fallback when user city or state missing

--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
-const UserInfo = ({user, message}) => {
+const UserInfo = ({ message, user }) => {
   const { fullName, twitterName, location } = user;
   const displayName = fullName || twitterName;
+  const locationString = location.city
+    ? location.state
+      ? ` in ${location.city}, ${location.state}`
+      : ` in ${location.city}`
+    : location.state ? ` in ${location.state}` : '';
   return (
     <div>
-      <img src={`https://twitter.com/${twitterName}/profile_image?size=normal`} role="presentation" />
+      <img
+        src={`https://twitter.com/${twitterName}/profile_image?size=normal`}
+        role="presentation"
+      />
       <br /><br />
-      {message} {displayName} in {location.city}, {location.state}.
+      {`${message} ${displayName}${locationString}`}.
       <br /><br />
     </div>
-  )
-}
+  );
+};
 
 export default UserInfo;
